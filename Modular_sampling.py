904c15934fd768ccdc36e7cca107043c5a25182d
mohdhasyim <mohdhasyim@uiowa.edu>
2023-11-26 21:53:32 -0600

    Code cleanup and Code documentation

--- Modular_sampling.py
+++ Modular_sampling.py
@@ -1,110 +1,6 @@
 import pandas as pd
 from sklearn.model_selection import train_test_split
 
-
-def adjust_distribution_test(sampled_data, category, target_distribution, total_samples, random_state=42):
-    """Adjust the distribution for a given category while maintaining unique data points."""
-    adjusted_samples = pd.DataFrame()
-    current_distribution = sampled_data[category].value_counts()
-
-    for group, target_percent in target_distribution.items():
-        required_samples = int(total_samples * target_percent / 100)
-        current_samples = current_distribution.get(group, 0)
-
-        group_data = sampled_data[sampled_data[category] == group]
-
-        # If current samples are less than required, add as many as possible up to the limit of unique data points
-        if current_samples < required_samples and len(group_data) > current_samples:
-            additional_samples = group_data.sample(n=(required_samples - current_samples),
-                                                   random_state=random_state, replace=False)
-            adjusted_samples = pd.concat(
-                [adjusted_samples, additional_samples])
-        elif current_samples > required_samples:
-            # Reduce samples if more than required
-            reduced_samples = group_data.sample(
-                n=required_samples, random_state=random_state, replace=False)
-            adjusted_samples = pd.concat([adjusted_samples, reduced_samples])
-        else:
-            # If the samples match the requirement, add them as is
-            adjusted_samples = pd.concat([adjusted_samples, group_data])
-
-    # Ensure the total count matches the intended sample size
-    if len(adjusted_samples) > total_samples:
-        adjusted_samples = adjusted_samples.sample(
-            n=total_samples, random_state=random_state)
-
-    return adjusted_samples
-
-# This method ensures that the datapoints taken are 3200. By filling it the rest of the datapool with random samples
-
-
-def adjust_distribution(sampled_data, original_data, category, target_distribution, total_samples, random_state=42):
-    """Adjust the distribution for a given category while maintaining unique data points and total sample size."""
-    # First, remove all samples of the specified category from the original dataset
-    remaining_data = original_data[~original_data.index.isin(
-        sampled_data.index)]
-
-    # Adjust the distribution within the sampled data
-    adjusted_samples = pd.DataFrame()
-    for group, target_percent in target_distribution.items():
-        required_samples = int(total_samples * target_percent / 100)
-        group_data = sampled_data[sampled_data[category] == group]
-        current_samples = len(group_data)
-
-        if current_samples > required_samples:
-            # Reduce samples if more than required
-            adjusted_group_data = group_data.sample(
-                n=required_samples, random_state=random_state, replace=False)
-        else:
-            # If the samples are less or equal to the requirement, add them as is
-            adjusted_group_data = group_data
-
-        adjusted_samples = pd.concat([adjusted_samples, adjusted_group_data])
-
-    # Add additional unique samples from remaining_data if total is less than 3200
-    if len(adjusted_samples) < total_samples:
-        additional_samples_needed = total_samples - len(adjusted_samples)
-        additional_samples = remaining_data.sample(
-            n=additional_samples_needed, random_state=random_state, replace=False)
-        adjusted_samples = pd.concat([adjusted_samples, additional_samples])
-
-    return adjusted_samples
-
-
-def adjust_distribution_in_batches(sampled_data, category, target_distribution, total_samples, batch_size=500, random_state=42):
-    """Adjust the distribution for a given category using batch processing."""
-    # Calculate the number of batches
-    num_batches = len(sampled_data) // batch_size + \
-        (0 if len(sampled_data) % batch_size == 0 else 1)
-
-    adjusted_samples = pd.DataFrame()
-
-    for batch_num in range(num_batches):
-        # Get the current batch
-        batch_start = batch_num * batch_size
-        batch_end = min((batch_num + 1) * batch_size, len(sampled_data))
-        batch_data = sampled_data.iloc[batch_start:batch_end]
-
-        # Adjust the distribution within this batch
-        current_distribution = batch_data[category].value_counts(
-            normalize=True) * total_samples
-        for group, target_percent in target_distribution.items():
-            required_samples = int(total_samples * target_percent / 100)
-            current_samples = int(current_distribution.get(group, 0))
-            if current_samples < required_samples:
-                additional_samples = batch_data[batch_data[category] == group].sample(
-                    n=(required_samples - current_samples), random_state=random_state, replace=True)
-            elif current_samples > required_samples:
-                additional_samples = batch_data[batch_data[category] == group].sample(
-                    n=required_samples, random_state=random_state)
-            else:
-                additional_samples = batch_data[batch_data[category] == group]
-
-            adjusted_samples = pd.concat(
-                [adjusted_samples, additional_samples])
-
-    return adjusted_samples
-
 # Load data from an Excel file
 
 def load_data(file_path):
@@ -246,7 +142,7 @@ def save_to_excel(sampled_data, target_distributions, output_file_path):
 # Example usage of the functions
 # Define file paths
 file_path = 'output_excel/test_file_30K__.xlsx'
-output_file_path = 'output_excel_v2/OIA_Output_4percenttolerance_3000Iterations_StateInc.xlsx'
+output_file_path = 'output_excel_v2/OIA_Output_4percenttolerance_10000Iterations_StateInc.xlsx'
 
 # Load data from the file
 data = load_data(file_path)
@@ -263,14 +159,7 @@ target_distributions = {
     'Employment': {'Retiree': 11, 'Self employed / Gig job': 18, 'In between employment': 3, 'Not yet employed': 3, 'Permanently employed': 65}
 }
 
-# for _ in range(50):  # For example, 50 iterations
-#     sampled_by_age = adjust_distribution(sampled_by_age, data , 'Ethnicity', target_distributions['Ethnicity'], total_samples)
-#     sampled_by_age = adjust_distribution(sampled_by_age, data , 'Area', target_distributions['Area'], total_samples)
-#     sampled_by_age = adjust_distribution(sampled_by_age, data, 'Age', target_distributions['Age'], total_samples )
-# sampled_by_age = adjust_distribution(sampled_by_age, data , 'Ethnicity', target_distributions['Ethnicity'], total_samples)
-# sampled_by_age = adjust_distribution(sampled_by_age, data , 'Area', target_distributions['Area'], total_samples)
-# adjusted_final = sampled_by_age
-# save_to_excel(adjusted_final, target_distributions, output_file_path)
+
 
 # Calculate total sample sizes for each category
 total_samples = 3200
@@ -287,7 +176,122 @@ categories = ['Ethnicity', 'Area', 'Age', 'State']
 
 # Perform iterative adjustment
 adjusted_final = iteratively_adjust_distribution(
-    sampled_by_age, data, categories, target_distributions, total_samples, max_iterations=3000, tolerance=0.04)
+    sampled_by_age, data, categories, target_distributions, total_samples, max_iterations=10000, tolerance=0.04)
 
 # Save the final adjusted data to an Excel file
 save_to_excel(adjusted_final, target_distributions, output_file_path)
+
+
+# def adjust_distribution_test(sampled_data, category, target_distribution, total_samples, random_state=42):
+#     """Adjust the distribution for a given category while maintaining unique data points."""
+#     adjusted_samples = pd.DataFrame()
+#     current_distribution = sampled_data[category].value_counts()
+
+#     for group, target_percent in target_distribution.items():
+#         required_samples = int(total_samples * target_percent / 100)
+#         current_samples = current_distribution.get(group, 0)
+
+#         group_data = sampled_data[sampled_data[category] == group]
+
+#         # If current samples are less than required, add as many as possible up to the limit of unique data points
+#         if current_samples < required_samples and len(group_data) > current_samples:
+#             additional_samples = group_data.sample(n=(required_samples - current_samples),
+#                                                    random_state=random_state, replace=False)
+#             adjusted_samples = pd.concat(
+#                 [adjusted_samples, additional_samples])
+#         elif current_samples > required_samples:
+#             # Reduce samples if more than required
+#             reduced_samples = group_data.sample(
+#                 n=required_samples, random_state=random_state, replace=False)
+#             adjusted_samples = pd.concat([adjusted_samples, reduced_samples])
+#         else:
+#             # If the samples match the requirement, add them as is
+#             adjusted_samples = pd.concat([adjusted_samples, group_data])
+
+#     # Ensure the total count matches the intended sample size
+#     if len(adjusted_samples) > total_samples:
+#         adjusted_samples = adjusted_samples.sample(
+#             n=total_samples, random_state=random_state)
+
+#     return adjusted_samples
+
+# # This method ensures that the datapoints taken are 3200. By filling it the rest of the datapool with random samples
+
+
+# def adjust_distribution(sampled_data, original_data, category, target_distribution, total_samples, random_state=42):
+#     """Adjust the distribution for a given category while maintaining unique data points and total sample size."""
+#     # First, remove all samples of the specified category from the original dataset
+#     remaining_data = original_data[~original_data.index.isin(
+#         sampled_data.index)]
+
+#     # Adjust the distribution within the sampled data
+#     adjusted_samples = pd.DataFrame()
+#     for group, target_percent in target_distribution.items():
+#         required_samples = int(total_samples * target_percent / 100)
+#         group_data = sampled_data[sampled_data[category] == group]
+#         current_samples = len(group_data)
+
+#         if current_samples > required_samples:
+#             # Reduce samples if more than required
+#             adjusted_group_data = group_data.sample(
+#                 n=required_samples, random_state=random_state, replace=False)
+#         else:
+#             # If the samples are less or equal to the requirement, add them as is
+#             adjusted_group_data = group_data
+
+#         adjusted_samples = pd.concat([adjusted_samples, adjusted_group_data])
+
+#     # Add additional unique samples from remaining_data if total is less than 3200
+#     if len(adjusted_samples) < total_samples:
+#         additional_samples_needed = total_samples - len(adjusted_samples)
+#         additional_samples = remaining_data.sample(
+#             n=additional_samples_needed, random_state=random_state, replace=False)
+#         adjusted_samples = pd.concat([adjusted_samples, additional_samples])
+
+#     return adjusted_samples
+
+
+# def adjust_distribution_in_batches(sampled_data, category, target_distribution, total_samples, batch_size=500, random_state=42):
+#     """Adjust the distribution for a given category using batch processing."""
+#     # Calculate the number of batches
+#     num_batches = len(sampled_data) // batch_size + \
+#         (0 if len(sampled_data) % batch_size == 0 else 1)
+
+#     adjusted_samples = pd.DataFrame()
+
+#     for batch_num in range(num_batches):
+#         # Get the current batch
+#         batch_start = batch_num * batch_size
+#         batch_end = min((batch_num + 1) * batch_size, len(sampled_data))
+#         batch_data = sampled_data.iloc[batch_start:batch_end]
+
+#         # Adjust the distribution within this batch
+#         current_distribution = batch_data[category].value_counts(
+#             normalize=True) * total_samples
+#         for group, target_percent in target_distribution.items():
+#             required_samples = int(total_samples * target_percent / 100)
+#             current_samples = int(current_distribution.get(group, 0))
+#             if current_samples < required_samples:
+#                 additional_samples = batch_data[batch_data[category] == group].sample(
+#                     n=(required_samples - current_samples), random_state=random_state, replace=True)
+#             elif current_samples > required_samples:
+#                 additional_samples = batch_data[batch_data[category] == group].sample(
+#                     n=required_samples, random_state=random_state)
+#             else:
+#                 additional_samples = batch_data[batch_data[category] == group]
+
+#             adjusted_samples = pd.concat(
+#                 [adjusted_samples, additional_samples])
+
+#     return adjusted_samples
+
+
+
+# for _ in range(50):  # For example, 50 iterations
+#     sampled_by_age = adjust_distribution(sampled_by_age, data , 'Ethnicity', target_distributions['Ethnicity'], total_samples)
+#     sampled_by_age = adjust_distribution(sampled_by_age, data , 'Area', target_distributions['Area'], total_samples)
+#     sampled_by_age = adjust_distribution(sampled_by_age, data, 'Age', target_distributions['Age'], total_samples )
+# sampled_by_age = adjust_distribution(sampled_by_age, data , 'Ethnicity', target_distributions['Ethnicity'], total_samples)
+# sampled_by_age = adjust_distribution(sampled_by_age, data , 'Area', target_distributions['Area'], total_samples)
+# adjusted_final = sampled_by_age
+# save_to_excel(adjusted_final, target_distributions, output_file_path)
